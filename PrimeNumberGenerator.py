cce4c938789f731f60a96914e45c8ddbbc93d739
iman008 <49917769+iman008@users.noreply.github.com>
2019-09-27 16:57:50 +0330

    Add files via upload

---
+++ PrimeNumberGenerator.py
@@ -0,0 +1,12 @@
+#Generating Prime numbers between 1 to 100
+lower = 1
+upper = 200
+
+for num in range(lower, upper+1):
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                break
+        else:
+            print(num)
+        
